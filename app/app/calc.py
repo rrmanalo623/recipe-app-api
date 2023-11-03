fe9a08a75c260968c72f77f6f7b8ed8e1b3dd3d4
Ray Manalo <rmanalo@liveintent.com>
2023-11-02 22:33:14 -0400

    feat: adding psycopg2 for py postgresql adapter

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator Function
+"""
+
+
+def add(x, y):
+    """Add x and y and returns results"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return results"""
+    return y - x
