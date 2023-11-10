8cb98d0ec6e4a13ddfa4cbdb8da162af76cadc49
Ray Manalo <rmanalo@liveintent.com>
2023-11-10 11:50:02 -0500

    feat: adding authentication

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,5 @@
 """Views for the user API"""
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -18,3 +18,14 @@ class CreateTokenView(ObtainAuthToken):
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user"""
+        return self.request.user
